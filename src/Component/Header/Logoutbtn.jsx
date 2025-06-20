fcfaadfeeb27d24cab0800a49fcb059a7f877636
Rohit Rathod <rohit.gr.rathod@gmail.com>
2025-06-20 12:36:47 +0530

    your message

--- src/Component/Header/Logoutbtn.jsx
+++ src/Component/Header/Logoutbtn.jsx
@@ -14,7 +14,7 @@ function Logoutbtn() {
 
   }
   return (
-    <button onClick={handleLogout} className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'>Logout</button>
+    <button onClick={handleLogout} className='inline-bock px-6 py-2 duration-200 !bg-red-500 rounded-full'>Logout</button>
   )
 }
 
